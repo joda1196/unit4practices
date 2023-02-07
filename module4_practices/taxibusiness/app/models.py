2391fb6929171de80bd6364ae5b446904cbb2741
joda1196 <jhtrott629@gmail.com>
2023-02-07 11:37:24 -0600

    taxi done

--- module4_practices/taxibusiness/app/models.py
+++ module4_practices/taxibusiness/app/models.py
@@ -13,49 +13,68 @@ class TaxiBusiness(models.Model):
     def is_occupied(self):
         return self.passengers != 0
 
-    @classmethod
-    def create_taxi(cls, capacity, passengers, fare, taxi_type, notes=""):
-        taxi = cls(
-            capacity=capacity,
-            passengers=passengers,
-            fare=fare,
-            taxi_type=taxi_type,
-            notes=notes,
-        )
+
+def create_taxi(capacity, passengers, fare, taxi_type, notes=""):
+    taxi = TaxiBusiness(
+        capacity=capacity,
+        passengers=passengers,
+        fare=fare,
+        taxi_type=taxi_type,
+        notes=notes,
+    )
+    taxi.occupied = taxi.is_occupied()
+
+    top = TaxiBusiness.objects.all().last()
+    if top != None:
+        taxi.taxi_number = top.taxi_number + 11
+    taxi.save()
+    return taxi
+
+
+def get_taxi(taxi_number):
+    try:
+        return TaxiBusiness.objects.get(taxi_number=taxi_number)
+    except TaxiBusiness.DoesNotExist:
+        raise ValueError("Not Found")
+
+
+def send_taxi(taxi_number, passengers):
+    taxi = get_taxi(taxi_number)
+    if passengers > taxi.capacity or taxi.occupied == True:
+        raise ValueError("Taxi occupied or too many.")
+    else:
+        taxi.passengers = passengers
         taxi.occupied = taxi.is_occupied()
+    taxi.save()
+    return taxi
 
-        top = cls.objects.all().last()
-        if top != None:
-            taxi.taxi_number = top.taxi_number + 11
-        taxi.save()
-        return taxi
 
-    @classmethod
-    def get_taxi(cls, taxi_number):
-        try:
-            return cls.objects.get(taxi_number=taxi_number)
-        except cls.DoesNotExist:
-            raise ValueError("Not Found")
+def end_fare(taxi_number, distance):
+    taxi = get_taxi(taxi_number)
+    if not taxi.occupied:
+        raise ValueError("Taxi is not on fare.")
+    else:
+        taxi.passengers = 0
+        taxi.occupied = taxi.is_occupied()
+    taxi.save()
+    return taxi.fare * distance
 
-    @classmethod
-    def send_taxi(cls, taxi_number, passengers):
-        taxi = cls.get_taxi(taxi_number)
-        if passengers > taxi.capacity or taxi.occupied == True:
-            raise ValueError("Taxi occupied or too many.")
-        else:
-            taxi.passengers = passengers
-            taxi.occupied = taxi.is_occupied()
-        taxi.save()
-        return taxi
 
-    def end_fare(cls, taxi_number, distance):
-        taxi = cls.get_taxi(taxi_number)
-        if not taxi.occupied:
-            raise ValueError("Taxi is not on fare.")
-        else:
-            taxi.passengers = 0
-            taxi.occupied = taxi.is_occupied()
-        taxi.save()
-        return taxi.fare * distance
+def remove_taxi(taxi_number):
+    TaxiBusiness.objects.get(taxi_number=taxi_number).delete()
 
-    # def remove_taxi():
+
+def find_taxi(taxi_number):
+    taxi = get_taxi(taxi_number)
+    return taxi
+
+
+def filter_unoccupied():
+    taxi = TaxiBusiness.objects.filter(occupied=False)
+    return taxi
+
+
+def filter_unoccupied_capacity(capacity=0):
+    if capacity != 0:
+        taxi = TaxiBusiness.objects.filter(capacity__gte=capacity, occupied=False)
+    return taxi
--- module4_practices/taxibusiness/app/tests.py
+++ module4_practices/taxibusiness/app/tests.py
@@ -1,42 +1,110 @@
 from django.test import TestCase
-from .models import TaxiBusiness
+from app import models
 
 
 class Test_TaxiBusiness(TestCase):
+    # ==========CREATE==========#
     def test_can_create(self):
-        taxi = TaxiBusiness.create_taxi(3, 2, 1.5, "van")
+        taxi = models.create_taxi(3, 2, 1.5, "van")
 
         self.assertEqual(taxi.passengers, 2)
-        self.assertEqual(taxi.occupied, True)
         self.assertEqual(taxi.taxi_number, 111)
         self.assertTrue(taxi.occupied)
 
-        taxi2 = TaxiBusiness.create_taxi(6, 0, 1.0, "car", "wait before loading")
+        taxi2 = models.create_taxi(6, 0, 1.0, "car", "wait before loading")
 
         self.assertEqual(taxi2.taxi_number, 122)
         self.assertEqual(taxi2.notes, "wait before loading")
-        taxi3 = TaxiBusiness.create_taxi(5, 0, 1.4, "van")
+
+        taxi3 = models.create_taxi(5, 0, 1.4, "van")
         self.assertEqual(taxi3.taxi_number, 133)
 
+    # ==========UPDATE==========#
     def test_can_update_taxi(self):
-        taxi = TaxiBusiness.create_taxi(3, 0, 1.5, "van")
-        taxi2 = TaxiBusiness.create_taxi(5, 3, 2, "car")
+        taxi = models.create_taxi(3, 0, 1.5, "van")
+        taxi2 = models.create_taxi(5, 3, 2, "car")
+
         self.assertFalse(taxi.occupied)
-        taxi = TaxiBusiness.send_taxi(taxi.taxi_number, 3)
+
+        taxi = models.send_taxi(taxi.taxi_number, 3)
+
         self.assertEqual(taxi.passengers, 3)
         self.assertTrue(taxi.occupied)
 
-        with self.assertRaises(ValueError):
-            TaxiBusiness.send_taxi(taxi.taxi_number, 5)
+    # ==========SEND==========#
+    def test_send_taxi(self):
+        taxi = models.create_taxi(3, 0, 1.5, "van")
+        taxi2 = models.create_taxi(5, 3, 2, "car")
 
         with self.assertRaises(ValueError):
-            TaxiBusiness.send_taxi(165, 4)
+            models.send_taxi(taxi.taxi_number, 5)
 
         with self.assertRaises(ValueError):
-            TaxiBusiness.send_taxi(taxi2.taxi_number, 3)
+            models.send_taxi(165, 4)
 
-    # def test_send_taxi(self):
-    #     taxi = TaxiBusiness.create_taxi(3, 0, 1.5, "van")
-    #     taxi2 = TaxiBusiness.create_taxi(5, 3, 2, "car")
-    #     taxi3 = TaxiBusiness.create_taxi(5, 0, 1.4, "van")
-    #     self.assertEqual(TaxiBusiness.)
+        with self.assertRaises(ValueError):
+            models.send_taxi(taxi2.taxi_number, 3)
+
+    # ==========END==========#
+    def test_end_fare(self):
+        taxi = models.create_taxi(3, 0, 1.5, "van")
+        taxi2 = models.create_taxi(5, 3, 2, "car")
+
+        self.assertEqual(models.end_fare(taxi2.taxi_number, 4.5), 9)
+
+        with self.assertRaises(ValueError):
+            models.end_fare(taxi.taxi_number, 3)
+
+        with self.assertRaises(ValueError):
+            models.end_fare(166, 3)
+
+        models.send_taxi(taxi.taxi_number, 3)
+
+        self.assertEqual(models.get_taxi(taxi.taxi_number).occupied, True)
+        self.assertEqual(models.get_taxi(taxi.taxi_number).passengers, 3)
+
+        models.end_fare(taxi.taxi_number, 5.0)
+
+        self.assertEqual(models.get_taxi(taxi.taxi_number).occupied, False)
+        self.assertEqual(models.get_taxi(taxi.taxi_number).passengers, 0)
+
+    # ==========DELETE TAXI==========#
+    def test_remove_taxi(self):
+        taxi = models.create_taxi(3, 0, 1.5, "van")
+        taxi2 = models.create_taxi(5, 3, 2, "car")
+        taxi3 = models.create_taxi(5, 0, 1.4, "van")
+
+        models.remove_taxi(taxi3.taxi_number)
+
+        with self.assertRaises(ValueError):
+            models.get_taxi(taxi3.taxi_number)
+
+        self.assertEqual(len(models.TaxiBusiness.objects.all()), 2)
+
+    # ==========FIND TAXI==========#
+    def test_find_taxi(self):
+        taxi = models.create_taxi(3, 0, 1.5, "van")
+        taxi2 = models.create_taxi(5, 3, 2, "car")
+        taxi3 = models.create_taxi(5, 0, 1.4, "van")
+
+        found = models.get_taxi(122)
+
+        self.assertIsNotNone(found)
+
+    # ==========FILTER UNOCCUPIED==========#
+    def test_filter_unoccupied(self):
+        taxi = models.create_taxi(3, 0, 1.5, "van")
+        taxi2 = models.create_taxi(5, 3, 2, "car")
+        taxi3 = models.create_taxi(5, 0, 1.4, "van")
+
+        self.assertIsNotNone(models.filter_unoccupied())
+        self.assertEqual(len(models.filter_unoccupied()), 2)
+
+    # ==========FILTER UNOCCUPIED & CAPACITY==========#
+    def test_filter_unoccupied(self):
+        taxi = models.create_taxi(3, 0, 1.5, "van")
+        taxi2 = models.create_taxi(5, 3, 2, "car")
+        taxi3 = models.create_taxi(5, 0, 1.4, "van")
+
+        self.assertIsNotNone(models.filter_unoccupied_capacity(4))
+        self.assertEqual(len(models.filter_unoccupied_capacity(4)), 1)
